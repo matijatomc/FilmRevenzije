bfa6b7380092352502b3bc9f5b33f72d3aad2626
polovni <luka.polovic11@gmail.com>
2024-02-16 20:46:12 +0100

    dodao logout, dodao filtirani prikaz komentara

--- tomcMatijaFilmRecenzije/main/urls.py
+++ tomcMatijaFilmRecenzije/main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'main'
@@ -6,7 +7,8 @@ app_name = 'main'
 urlpatterns = [
     path('', views.home),
     path('signup/', views.signup_view),
-    path('login/', views.login_view),
+    path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='main:login'), name='logout'),
     path('film/', views.FilmView.as_view()),
-    path('recenzija/', views.RecenzijaView.as_view())
+    path('recenzija/<int:movie_id>/', views.RecenzijaView.as_view(), name='recenzije')
 ]
